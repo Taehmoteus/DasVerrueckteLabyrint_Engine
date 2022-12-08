7655753f1f0cf031c5c60a5f5b5fa8722705cab3
tstoehr <@4KB%$3ysM$d3!6%od5!>
2022-12-08 14:25:29 +0100

    Change Files

---
+++ src/Connection.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+/**
+ * The class creates the engine
+ * @author tstoehr
+ */
+public class Connection {
+
+    public static Engine engine;
+
+    /**
+     * Create a name for the engine, creates it and runs it
+     *
+     * @param args input arguments from the console
+     * @author tstoehr
+     */
+    public static void main(String[] args) {
+        //Create a random Number between 0 and 1000000 for the signature of the engine
+        Scanner inputArguments = new Scanner(System.in);
+        System.out.println("Enter IP: ");
+        String serverIp = inputArguments.nextLine();
+        System.out.println("Enter Port: ");
+        String serverPort = inputArguments.nextLine();
+        System.out.println("Enter Username: ");
+        String engineUsername = inputArguments.nextLine();
+
+        engine = new Engine(serverIp, Integer.parseInt(serverPort), engineUsername);
+        //TODO change input variables
+        try {
+            engine.run();
+        } catch (IOException e) {
+            System.out.println("Unexpected error: " + e);
+            System.exit(0);
+        }
+    }
+}
